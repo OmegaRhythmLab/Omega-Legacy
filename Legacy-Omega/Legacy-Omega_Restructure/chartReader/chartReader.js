238d78a26d431753da0ad1a4d336b02dfef56210
BallThe <1539110121@qq.com>
2022-11-26 23:03:06 +0800

    #

--- Legacy-Omega/Legacy-Omega_Restructure/chartReader/chartReader.js
+++ Legacy-Omega/Legacy-Omega_Restructure/chartReader/chartReader.js
@@ -15,6 +15,35 @@ var openFile = function(event){
 
 // 主函数
 function main(chart){
+    // chart转为列表
     chart = chart.split("\n")
     document.getElementById("chart").innerHTML = chart
+    // OMEGA标识符
+    if (chart.indexOf("!ChartType OMEGA") != -1){
+        // 铺面信息
+        chartInfo = {
+            bpm: 0,
+            bpm_ms: 0,
+            notespeed: [],
+            artist: "",
+            chart: "",
+            name: "",
+            level: "",
+            combos: "",
+            score_per_one: "",
+        }
+        // 遍历
+        for(var i=0; i++; i>=chart.length-1){
+            var item = chart[i]
+            // bpm notespeed ... 唯一标识符
+            switch(item){
+                case "bpm":
+                    chartInfo[bpm] = parseInt(chart[i+1])
+                    chartInfo[bpm_ms] = 60/parseInt(chart[i+1])*1000
+                case "notespeed":
+                    chartInfo[notespeed].push()
+            }
+        }
+    }
+    
 }
