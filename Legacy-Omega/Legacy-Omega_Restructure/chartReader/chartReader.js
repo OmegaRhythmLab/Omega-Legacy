f4769daec64f083555ab92aee3bf00c82c81aafb
BallThe <1539110121@qq.com>
2022-11-27 11:59:23 +0800

    #

--- Legacy-Omega/Legacy-Omega_Restructure/chartReader/chartReader.js
+++ Legacy-Omega/Legacy-Omega_Restructure/chartReader/chartReader.js
@@ -17,9 +17,8 @@ var openFile = function(event){
 function main(chart){
     // chart转为列表
     chart = chart.split("\n")
-    document.getElementById("chart").innerHTML = chart
     // OMEGA标识符
-    if (chart.indexOf("!ChartType OMEGA") != -1){
+    if (chart.indexOf("!ChartType OMEGA\r") != -1){
         // 铺面信息
         chartInfo = {
             bpm: 0,
@@ -31,18 +30,33 @@ function main(chart){
             level: "",
             combos: "",
             score_per_one: "",
+            notes: [],
+            time_all: 0,
         }
-        // 遍历
-        for(var i=0; i++; i>=chart.length-1){
-            var item = chart[i]
-            // bpm notespeed ... 唯一标识符
-            switch(item){
+        // 遍历chart获取信息
+        for(var i=0; i<=chart.length-1; i++){
+            var item = chart[i].replace("\r","")
+            // bpm ... 唯一标识符
+            switch (item){
+                // bpm, bpm_ms
                 case "bpm":
-                    chartInfo[bpm] = parseInt(chart[i+1])
-                    chartInfo[bpm_ms] = 60/parseInt(chart[i+1])*1000
-                case "notespeed":
-                    chartInfo[notespeed].push()
+                    chartInfo["bpm"] = parseInt(chart[i+1])
+                    chartInfo["bpm_ms"] = parseInt(60/parseInt(chart[i+1])*1000)
+                // notespeed, time_all, notes...
+                default:
+                    // n/n类型的数据
+                    if (item.indexOf("/") == 1){
+                        item = item.split(" ")
+                        // time_all 所有时间(ms)
+                        chartInfo["time_all"] += parseInt(item[0][0])/parseInt(item[0][2])*parseInt(chartInfo["bpm_ms"])
+                        // 长度为2的数据后都带有notespeed属性(ms)
+                        if (item.length == 2){
+                            // notespeed
+                            chartInfo["notespeed"].push([chartInfo["time_all"],parseInt(item[1])])
+                        }
+                    }
             }
+        document.getElementById("chart").innerHTML = JSON.stringify(chartInfo)
         }
     }
 }
